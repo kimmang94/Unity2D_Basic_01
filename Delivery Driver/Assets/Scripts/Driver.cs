01cb79f253f6993c1a2a1bc020172389fabbb64a
kimhyeok <kimmang940715@naver.com>
2023-08-28 05:40:12 +0900

    속도 조절을 위한 변수 추가

--- Delivery Driver/Assets/Scripts/Driver.cs
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    private float steerSpeed = 0.1f;
+    private float moveSpeed = 0.01f;
+    
+    
     private void Start()
     {
     }
 
     private void Update()
     {
-        transform.Rotate(0f, 0f, 0.1f);
-        transform.Translate(0f, 0.01f, 0f);
+        transform.Rotate(0f, 0f, steerSpeed);
+        transform.Translate(0f, moveSpeed, 0f);
     }
     
     
