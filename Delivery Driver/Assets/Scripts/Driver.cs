bd7642cb6391de57239c5b3cc9c23840898d3e33
kimhyeok <kimmang940715@naver.com>
2023-08-28 09:07:47 +0900

    project Settings -> Input Manager 에서 Input Manager 의 값을 변경할수 있다

--- Delivery Driver/Assets/Scripts/Driver.cs
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField]
-    private float steerSpeed = 0.1f;
+    private float steerSpeed = 0.5f;
     [SerializeField]
     private float moveSpeed = 0.01f;
     
@@ -16,8 +16,10 @@ public class Driver : MonoBehaviour
 
     private void Update()
     {
-        transform.Rotate(0f, 0f, steerSpeed);
-        transform.Translate(0f, moveSpeed, 0f);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Rotate(0f, 0f, -steerAmount);
+        transform.Translate(0f, moveAmount, 0f);
     }
     
     
