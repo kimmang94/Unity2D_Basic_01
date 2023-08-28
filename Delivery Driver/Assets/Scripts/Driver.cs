0634c29870e4446a5febfe7ce6bb89a8f09ed0a1
kimhyeok <kimmang940715@naver.com>
2023-08-28 09:24:21 +0900

    Time.deltaTime 10프레임 초당 0.1프레임  1 x 10 x0.1 = 1 100프레임 초당 0.01프레임 1 x 100 x 0.01 = 1 로 모든 사양에서 같은 속도로 맞춰준다

--- Delivery Driver/Assets/Scripts/Driver.cs
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Driver : MonoBehaviour
 {
     [SerializeField]
-    private float steerSpeed = 0.5f;
+    private float steerSpeed = 200f;
     [SerializeField]
-    private float moveSpeed = 0.01f;
+    private float moveSpeed = 10f;
     
     
     private void Start()
@@ -16,8 +16,8 @@ public class Driver : MonoBehaviour
 
     private void Update()
     {
-        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0f, 0f, -steerAmount);
         transform.Translate(0f, moveAmount, 0f);
     }
