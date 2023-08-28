f8b22b4c7fa1920f276fa59279d1f81cb4af58ea
kimhyeok <kimmang940715@naver.com>
2023-08-28 23:41:19 +0900

    물품픽업, 배달완료 Trigger 추가

--- Delivery Driver/Assets/Scripts/Player/DriverTrigger.cs
+++ Delivery Driver/Assets/Scripts/Player/DriverTrigger.cs
@@ -5,8 +5,29 @@ using UnityEngine;
 
 public class DriverTrigger : MonoBehaviour
 {
+    
+    private bool hasPackage = false;
+    
+    /// <summary>
+    /// other 와 Trigger 시 Package 이면 물품픽업 hasPackage = true,
+    /// Customer 이면 물품 배달완료 hasPackage = false
+    /// </summary>
+    /// <param name="other"></param>
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("트리거!!");
+        if (other.gameObject.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("물품 픽업 완료!");
+            other.gameObject.SetActive(false);
+            hasPackage = true;
+        }
+
+        if (other.gameObject.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("배달 완료!");
+            hasPackage = false;
+        }
+        
     }
+
 }
