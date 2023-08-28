d4e534284acf325c2b819da91e7bcf3100362490
kimhyeok <kimmang940715@naver.com>
2023-08-28 09:41:03 +0900

    플레이어 스크립트 이동

---
+++ Delivery Driver/Assets/Scripts/Player/Driver.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    [SerializeField]
+    private float steerSpeed = 200f;
+    [SerializeField]
+    private float moveSpeed = 10f;
+    
+    
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0f, 0f, -steerAmount);
+        transform.Translate(0f, moveAmount, 0f);
+    }
+    
+    
+}
